bd3419bfa4a8a2925b2c01e756d9f7ebfc00a3e9
Ratnesh Karbhari <ratneshkarbhari18@gmail.com>
2023-02-25 14:45:39 +0530

    notes fetched and displayed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+
+import axios from "axios";
 
 function App() {
+
+  const [notes,setNotes] = useState(null);
+
+  useEffect(()=>{
+
+    fetchNotes();
+
+    
+
+  },[])
+
+  const fetchNotes = async() => {
+
+    let res = await axios.get("http://localhost:3000/notes")
+
+    setNotes(res.data.notes);
+
+
+
+    console.log(res)
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Notes:</h2>
+      {notes && notes.map(note=>{
+        return <div key={note._id }>
+
+          <h3>{note.title}</h3>
+
+        </div>
+      })}
     </div>
   );
 }
